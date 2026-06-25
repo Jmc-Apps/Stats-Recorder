dd34f5dffb348cca8e5dfac7879f306d7a741cab
Jmc-Apps <27j7r6nmhw@privaterelay.appleid.com>
2026-06-25 12:58:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='goalie-stats-recorder-v1-24';
-const FILES=['./','./index.html','./manifest.webmanifest','./assets/d_heat_map.png','./assets/goal_box_heat_map.png'];
+const CACHE_NAME='goalie-stats-recorder-v1-25';
+const FILES=['./','./index.html','./manifest.webmanifest','./favicon-32.png','./apple-touch-icon.png','./icon-512.png','./icon-192.png','./assets/d_heat_map.png','./assets/goal_box_heat_map.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
