1ff59e696901f3a6b58b58bb8f5db489f47e7bc0
Jmc-Apps <27j7r6nmhw@privaterelay.appleid.com>
2026-06-25 12:14:02 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='goalie-stats-recorder-v1-08';
-const FILES_TO_CACHE=['./','./index.html','./goalie_stats_recorder_v1_08.html','./manifest.webmanifest','./manifest.json','./service-worker.js','./assets/title-logo.png','./assets/d.svg','./assets/box.svg','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/favicon-32.png','./icons/apple-touch-icon.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
+const CACHE_NAME='goalie-stats-recorder-v1-19';
+const FILES=['./','./index.html','./manifest.webmanifest','./assets/d_heat_map.png','./assets/goal_box_heat_map.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
